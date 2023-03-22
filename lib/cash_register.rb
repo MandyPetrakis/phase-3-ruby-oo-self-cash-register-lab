9081d63af3e336996f8e5ed24ce36b4f21f262fa
Mandy Petrakis <mandypetrakis@gmail.com>
2023-03-22 11:10:02 -0700

    commit tree 21ba7ab67aa0e8f7a5f9eb34bfce4afc517dc328

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,29 @@
+class CashRegister
+    attr_accessor :discount, :total, :items, :last_transaction 
+    def initialize(discount = nil)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+    def add_item(title, price, quantity=1)
+        @total += price * quantity 
+        @last_transaction = price * quantity
+        i = 0
+        while i < quantity
+        @items << title 
+        i += 1
+        end
+    end
+    def apply_discount 
+        if @discount == nil
+            return "There is no discount to apply."
+        else 
+        @total -=  @total * @discount / 100
+        "After the discount, the total comes to $#{@total}."
+        end
+    end  
+    def void_last_transaction
+        @total -= @last_transaction
+    end
+end
 
